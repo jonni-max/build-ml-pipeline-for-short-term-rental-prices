4447fd838971928b884c51b71aede85aa86ff023
jonni-max <122572277+jonni-max@users.noreply.github.com>
2023-03-18 19:04:41 +0100

    Added geolocation filtering

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -27,6 +27,10 @@ def go(args):
     idx = df['price'].between(args.min_price, args.max_price)
     df = df[idx].copy()
     
+    logger.info("Filtering offers with respect to geolocation")
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+    
     logger.info("Saving clean artifact and uploading")
     df.to_csv("clean_sample.csv", index=False)
     
